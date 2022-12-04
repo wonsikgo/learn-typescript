5c9c14008ae9ae34b7d62d572f1358483d2ec524
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-12-04 23:35:42 +0900

    계산기 함수 만들기

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -7,16 +7,21 @@ console.log(calculate("multiply", 4, 2)); // 8
 console.log(calculate("divide", 4, 2)); // 2
 console.log(calculate("remainder", 5, 2)); // 1
 
-function calculate(type: string, num1: number, num2: number): number {
-  if (type === "add") {
-    return num1 + num2;
-  } else if (type === "substract") {
-    return num1 - num2;
-  } else if (type === "multiply") {
-    return num1 * num2;
-  } else if (type === "divide") {
-    return num1 / num2;
-  } else if (type === "remainder") {
-    return num1 % num2;
+type Command = "add" | "substract" | "multiply" | "divide" | "remainder";
+
+function calculate(command: Command, num1: number, num2: number): number {
+  switch (command) {
+    case "add":
+      return num1 + num2;
+    case "substract":
+      return num1 - num2;
+    case "multiply":
+      return num1 * num2;
+    case "divide":
+      return num1 / num2;
+    case "remainder":
+      return num1 % num2;
+    default:
+      throw new Error("unknown error");
   }
 }
