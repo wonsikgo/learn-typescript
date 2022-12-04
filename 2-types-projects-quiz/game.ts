9f88a86ef02c80872a54ce1bd31d04de760854dd
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-12-04 23:52:33 +0900

    좌표 게임

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,42 @@
 /**
  * Let's make a game 🕹
  */
+
+type Direction = "up" | "down" | "left" | "right";
+type Position = {
+  x: number;
+  y: number;
+};
+let position: Position = {
+  x: 0,
+  y: 0,
+};
+
+function move(direction: Direction): void {
+  switch (direction) {
+    case "up":
+      position.y++;
+      break;
+    case "down":
+      position.y--;
+      break;
+    case "left":
+      position.x--;
+      break;
+    case "right":
+      position.x++;
+      break;
+    default:
+      throw new Error(`unknown direction : ${direction}`);
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
