9733c598d8c3c0ddcbb699ace00d686ef4544e45
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-12-06 14:16:00 +0900

    로딩 상태 표시

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -3,24 +3,34 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
+
+  function printLoginState(resourceLoadState: ResourceLoadState): void {
+    if (resourceLoadState.state === "loading") {
+      console.log(`👀 ${resourceLoadState.state}...`);
+    } else if (resourceLoadState.state === "success") {
+      console.log(`😃 ${resourceLoadState.response.body}`);
+    } else if (resourceLoadState.state === "fail") {
+      console.log(`😱 ${resourceLoadState.reason}`);
+    }
+  }
 }
