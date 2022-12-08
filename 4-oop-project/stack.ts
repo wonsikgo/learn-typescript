751fd06bc48783c0e01f08a775c2c4ac0dd152b0
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-12-08 14:35:32 +0900

    Stack 구현 수정

--- 4-oop-project/stack.ts
+++ 4-oop-project/stack.ts
@@ -4,43 +4,47 @@ interface Stack {
   pop(): string;
 }
 
-class List implements Stack {
-  private index: number = 0;
-  private array: string[] = new Array(this.size);
+type StackNode = {
+  readonly value: string;
+  readonly next?: StackNode;
+};
 
-  constructor(readonly size: number) {}
+class StackImpl implements Stack {
+  private _size: number = 0;
+  private head?: StackNode;
+
+  constructor(private capacity: number) {}
+
+  get size() {
+    return this._size;
+  }
 
   push(value: string): void {
-    if (typeof value !== "string") {
-      throw new Error(`${value} 문자열이 아닙니다.`);
+    if (this.size === this.capacity) {
+      throw new Error("Stack is full");
     }
 
-    if (this.size <= this.index) {
-      throw new Error("더 이상 문자열을 담을 수 없습니다.");
-    }
-
-    this.array[this.index] = value;
-    this.index++;
+    const node: StackNode = { value, next: this.head };
+    this.head = node;
+    this._size++;
   }
   pop(): string {
-    if (this.index === 0) {
-      throw new Error("List가 비어있습니다.");
+    if (this.head == null) {
+      throw new Error("Stack is Empty");
     }
-    this.index--;
-    return this.array[this.index];
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.value;
   }
 }
 
-const list = new List(10);
-list.push("테스트1");
-list.push("테스트2");
-list.push("테스트3");
-list.push("테스트4");
-list.push("테스트5");
+const stack = new StackImpl(10);
+stack.push("Ellie 1");
+stack.push("Bob 1");
+stack.push("Steve 1");
 
-console.log(list.pop());
-console.log(list.pop());
-console.log(list.pop());
-console.log(list.pop());
-console.log(list.pop());
-console.log(list.pop());
+while (stack.size !== 0) {
+  console.log(stack.pop());
+}
+// stack.pop();
