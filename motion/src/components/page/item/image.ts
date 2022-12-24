6b632a042575a9745cc9656bfd00aee3cc84fe23
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-12-25 00:26:49 +0900

    컴포넌트 리팩토링

--- motion/src/components/page/item/image.ts
+++ motion/src/components/page/item/image.ts
@@ -1,13 +1,11 @@
-export class ImageComponent {
-  private element: HTMLElement;
-  constructor(title: string, url: string) {
-    const template = document.createElement("template");
-    template.innerHTML = `<section class="image">
-        <div class="image__holder"><img class="image_thumbnail" /></div>
-        <p class="image__title"></p>
-    </section>`;
-    this.element = template.content.firstElementChild! as HTMLElement;
+import { BaseComponent } from "../../component.js";
 
+export class ImageComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="image">
+                <div class="image__holder"><img class="image_thumbnail" /></div>
+                <p class="image__title"></p>
+            </section>`);
     const imageElement = this.element.querySelector(
       ".image_thumbnail"
     )! as HTMLImageElement;
@@ -18,9 +16,5 @@ export class ImageComponent {
       ".image__title"
     )! as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
