de81ded623ccdaafa78e89f6831c45ec0b9a2809
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-12-31 23:51:18 +0900

    page component로 감싸기

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,10 +1,12 @@
+import { Component } from "./components/component.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent } from "./components/page/page.js";
+
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
@@ -13,20 +15,24 @@ class App {
       "Image Title",
       "https://picsum.photos/600/300"
     );
-    image.attachTo(appRoot, "beforeend");
+    this.page.addChild(image);
+    // image.attachTo(appRoot, "beforeend");
 
     const note = new NoteComponent("Note Title", "Note Body");
-    note.attachTo(appRoot, "beforeend");
+    this.page.addChild(note);
+    // note.attachTo(appRoot, "beforeend");
 
     const todo = new TodoComponent("Todo Title", "Todo Item");
-    todo.attachTo(appRoot, "beforeend");
+    this.page.addChild(todo);
+    //todo.attachTo(appRoot, "beforeend");
 
     const video = new VideoComponent(
       "Video Title",
       "https://www.youtube.com/embed/BoTVJvvXNnc?list=RDo9SujOQXfgk"
     );
+    this.page.addChild(video);
 
-    video.attachTo(appRoot, "beforeend");
+    // video.attachTo(appRoot, "beforeend");
   }
 }
 
